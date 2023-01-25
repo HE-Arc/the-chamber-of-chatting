a32b9c8e7dbce3fa82a7748f2e08c12f59451b23
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-01-25 16:43:03 +0100

    Initial commit

---
+++ api/the_chamber_of_chatting_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TheChamberOfChattingAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "the_chamber_of_chatting_app"
