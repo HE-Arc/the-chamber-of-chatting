0e43254f93cd0849a6b78b0d982cbaf5e6150ae6
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-02-22 16:03:47 +0100

    add views for topic and message #5 #7

--- api/the_chamber_of_chatting_app/views.py
+++ api/the_chamber_of_chatting_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 
+from rest_framework import viewsets
+
+
+
+from .models import Topic, Message
+from .serializers import TopicSerializer, MessageSerializer, UserSerializer
 # Create your views here.
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class TopicViewSet(viewsets.ModelViewSet):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+    
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
