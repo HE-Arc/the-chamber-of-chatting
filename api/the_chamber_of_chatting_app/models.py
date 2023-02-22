655efa3bfb13b9fb7cb656f8ec85fd83f88f57fa
Besia Alexandre <alexandre.besia@he-arc.ch>
2023-02-22 15:44:58 +0100

    Topic and Message Tables

--- api/the_chamber_of_chatting_app/models.py
+++ api/the_chamber_of_chatting_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Topic(models.Model):
+    id = models.Index()
+    topic_name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    
+class Message(models.Model):
+    id = models.Index()
+    user_id = models.ForeignKey(User, related_name="messages", on_delete=models.SET_NULL) # if user is banned, messages stay
+    topic_id = models.ForeignKey(Topic, related_name="topic", on_delete=models.CASCADE) # if topic is deleted, messages are deleted too
+    message = models.CharField(max_length=5000)
+    created = models.DateTimeField(auto_now_add=True)
