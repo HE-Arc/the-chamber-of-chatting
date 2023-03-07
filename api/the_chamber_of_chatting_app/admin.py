6816da256effec4204d6253390f379ca8a1afde4
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-03-07 16:51:29 +0100

    add white backbround

--- api/the_chamber_of_chatting_app/admin.py
+++ api/the_chamber_of_chatting_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Message, Topic
+
 # Register your models here.
+
+class MessageInline(admin.TabularInline):
+    model = Message
+    extra = 0
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("topic_name", "created")
+    list_filter = ("created",)
+    search_fields = ("topic_name",)
+    
+    inlines = [MessageInline]
+
+admin.site.register(Topic, TopicAdmin)
+
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -14,7 +14,7 @@ const submit = async () => {
     await axios.post("/topics/", {
       topic_name: name.value,
     });
-    name.value = "";
+    name.value = null;
     fetchTopics();
   } catch (error) {
     errors.value = error.response.data;
@@ -31,21 +31,23 @@ onMounted(() => {
 <template>
   {{ errors }}
   <q-page padding>
-    <q-form @submit="submit()" class="q-gutter-md">
-      <q-input
-        filled
-        v-model="name"
-        label="Topic Name *"
-        lazy-rules
-        :rules="[
-          (val) =>
-            (val && val.length > 0) || 'Please give a name to your topic',
-        ]"
-      />
-      <div>
-        <q-btn label="Submit" type="submit" color="primary" />
-      </div>
-    </q-form>
+    <div class="q-pa-md bg-white" style="max-width: 400px">
+      <q-form @submit="submit()" class="q-gutter-md">
+        <q-input
+          filled
+          v-model="name"
+          label="Topic Name *"
+          lazy-rules
+          :rules="[
+            (val) =>
+              (val && val.length > 0) || 'Please give a name to your topic',
+          ]"
+        />
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" />
+        </div>
+      </q-form>
+    </div>
     <q-list bordered>
       <q-item v-for="topic in topics" :key="topic.id">
         <q-item-section>
