a99a88c9cdd271be32c8c5446674121a6f639774
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-03-08 12:50:16 +0100

    define default  baseURL for axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,8 @@ import "quasar/dist/quasar.css";
 
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL;
+axios.defaults.baseURL =
+  import.meta.env.VITE_APP_API_URL || "http://127.0.0.1:8000/api"; // default when .env is not set
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.xsrfCookieName = "csrftoken";
 
