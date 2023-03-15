d249b255a598da5c7b8f3fbb96204728812de0e9
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-03-15 15:18:16 +0100

    move .env to root foder

--- api/the_chamber_of_chatting/settings.py
+++ api/the_chamber_of_chatting/settings.py
@@ -15,10 +15,12 @@ import environ
 
 # Initialize environment variables
 env = environ.Env()
-environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(BASE_DIR/ ".env")
+
 
 
 # Quick-start development settings - unsuitable for production
