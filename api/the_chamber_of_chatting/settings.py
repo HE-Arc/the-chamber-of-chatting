cdf16d18a715c7e4fc84ac65dfda403f0b56bf74
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-03-22 14:38:37 +0100

    add login route

--- api/the_chamber_of_chatting/settings.py
+++ api/the_chamber_of_chatting/settings.py
@@ -151,3 +151,14 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Django Rest Framework
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.SessionAuthentication',
+#     ],
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',  
+#     ],
+# }
--- api/the_chamber_of_chatting_app/views.py
+++ api/the_chamber_of_chatting_app/views.py
@@ -1,23 +1,40 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from rest_framework import viewsets
+from .models import Message, Topic
+from .serializers import MessageSerializer, TopicSerializer, UserSerializer
 
-
-
-from .models import Topic, Message
-from .serializers import TopicSerializer, MessageSerializer, UserSerializer
 # Create your views here.
+
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint that allows users to be viewed or edited."""
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+
+    @action(detail=False, methods=["POST"], url_path="login")
+    def user_login(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            return Response({"error": "Please provide both username and password"}, status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return Response(status=status.HTTP_200_OK)
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
 class TopicViewSet(viewsets.ModelViewSet):
     """API endpoint that allows topics to be viewed or edited."""
+
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
-    
+
+
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
