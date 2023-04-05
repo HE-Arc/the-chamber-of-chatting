5865fd3f7925dfccb92d4b0df55833e63ce243ef
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-04-05 12:21:23 +0200

    add redirection after new topic creation

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -19,19 +19,21 @@ onMounted(() => {
   <q-page padding>
     <div class="row self-center justify-evenly">
       <div class="col-12 q-mt-md">
-		<q-btn color="primary" :to="{ name: 'topics.create' }"> New Topic </q-btn>
+        <q-btn color="primary" :to="{ name: 'topics.create' }">
+          New Topic
+        </q-btn>
       </div>
     </div>
     <div class="row self-center justify-evenly">
       <div class="col-12 q-mt-md">
         <q-card class="q-pa-lg">
-			<q-list bordered>
-        <q-item v-for="topic in topics" :key="topic.id">
-					<q-item-section>
-					  <q-item-label>{{ topic.topic_name }}</q-item-label>
-					</q-item-section>
-				  </q-item>
-				</q-list>
+          <q-list bordered>
+            <q-item v-for="topic in topics" :key="topic.id">
+              <q-item-section>
+                <q-item-label>{{ topic.topic_name }}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
         </q-card>
       </div>
     </div>
