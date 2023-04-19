33f687a4764fdd923e60efad5a8721b93efcc1b5
Besia Alexandre <alexandre.besia@he-arc.ch>
2023-04-19 13:53:47 +0200

    topic now show all messages

--- frontend/src/views/TopicView.vue
+++ frontend/src/views/TopicView.vue
@@ -4,7 +4,8 @@ import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 
 const topic = ref([]);
-const route = useRoute();
+const messages = ref([]);
+const route=useRoute();
 
 const fetchTopic = async () => {
   const response = await axios.get("/topics/" + route.params.id);
@@ -17,21 +18,32 @@ onMounted(() => {
 </script>
 
 <template>
-  <q-page padding>
-    <q-btn color="primary" @click="this.$router.back()">
-      <q-icon left name="arrow_back" />
-      <div>Back</div>
-    </q-btn>
-    <div class="row self-center justify-evenly">
-      <div class="col-12 q-mt-md">
-        <q-card class="q-pa-lg">
-          <q-card-section class="text-center">
-            <div class="text-h5">{{ topic.topic_name }}</div>
-          </q-card-section>
-        </q-card>
-      </div>
-    </div>
-  </q-page>
-</template>
+    <q-page padding>
+      <q-btn color="primary" @click="this.$router.back()">
+        <q-icon left name="arrow_back" />
+        <div>Back</div>
+      </q-btn>
+        <div class="row self-center justify-evenly">
+          <div class="col-12 q-mt-md">
+            <q-card class="q-pa-lg">
+  
+              <q-card-section class="text-center">
+                <div class="text-h5">{{ topic.topic_name }}</div>
+              </q-card-section>
 
-<style scoped></style>
+              <q-list bordered>
+                <q-item v-for="msg in topic.messages" :key="msg.id">
+                    <q-item-section>
+                        <q-item-label>{{ msg.message }}</q-item-label>
+                    </q-item-section>
+                    </q-item>
+                </q-list>
+
+            </q-card>
+          </div>
+        </div>
+    </q-page>
+  </template>
+  
+  <style scoped></style>
+  
