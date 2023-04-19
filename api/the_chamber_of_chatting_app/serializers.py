a180f057fd66d882eb81244da6fefb81f9251bc6
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-04-19 13:14:34 +0200

    improve serializer

--- api/the_chamber_of_chatting_app/serializers.py
+++ api/the_chamber_of_chatting_app/serializers.py
@@ -39,6 +39,8 @@ class MessageSerializer(serializers.HyperlinkedModelSerializer):
         fields = [
             "id",
             "url",
+            "user_id",
+            "topic_id",
             "message",
             "created",
         ]
