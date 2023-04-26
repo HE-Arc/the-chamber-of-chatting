8b996e079ad5740d12263dcb74980977e9ad1bdc
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-04-26 12:45:57 +0200

    fix logout api

--- api/the_chamber_of_chatting_app/views.py
+++ api/the_chamber_of_chatting_app/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 
 from .models import Message, Topic
 from .serializers import MessageSerializer, TopicSerializer, UserSerializer
+
 # Create your views here.
 
 
@@ -17,38 +18,48 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
     @action(detail=False, methods=["POST"], url_path="login")
     def user_login(self, request):
-        print(request)
         username = request.data.get("username")
         password = request.data.get("password")
         print(f"fetch data, username: {username}, password: {password}")
         if not username or not password:
-            return Response({"error": "Please provide both username and password"}, status=status.HTTP_400_BAD_REQUEST)
-        print("check data")
+            return Response(
+                {"error": "Please provide both username and password"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         user = authenticate(username=username, password=password)
-        print("authenticate")
+
         if user is not None:
             login(request, user)
-            return Response(status=status.HTTP_200_OK)
-            print("login")
-        return Response({"error": "Invalid credentials"}, status=status.HTTP_403_FORBIDDEN)
-    
+            return Response(data=user, status=status.HTTP_200_OK)
+
+        return Response(
+            {"error": "Invalid credentials"}, status=status.HTTP_403_FORBIDDEN
+        )
+
     @action(detail=False, methods=["POST"], url_path="register")
     def user_register(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
         check_password = request.data.get("check_password")
         if not username or not password or not check_password:
-            return Response({"error": "Please provide all fields"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Please provide all fields"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         if password != check_password:
-            return Response({"error": "Passwords do not match"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Passwords do not match"}, status=status.HTTP_400_BAD_REQUEST
+            )
         user = User.objects.create_user(username=username, password=password)
-        login (request, user)
-        return Response(status=status.HTTP_200_OK)
-    
+        login(request, user)
+        return Response(data=user, status=status.HTTP_200_OK)
+
     @action(detail=False, methods=["POST"], url_path="logout")
-    def logout_view(request):
+    def logout_view(self, request):
         logout(request)
         return Response(status=status.HTTP_200_OK)
+
 
 class TopicViewSet(viewsets.ModelViewSet):
     """API endpoint that allows topics to be viewed or edited."""
